fabb4f5ec1383b40e3f27d0c0a7f9b3a3cb310a6
ISIKALU01 <davisleevtobi@gmail.com>
2023-06-17 06:58:32 +0100

    add async weather api js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+const endpoint = 'http://api.weatherapi.com/v1/current.json?key=1bf8f4b95ad14cf186b162118230606 &q=london&aqi=no'
+const response = await fetch(endpoint, { mode: "cors" });
+const data = await response.json();
+console.log(data)
 
